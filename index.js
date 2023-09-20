b93491450c06f9cd71692a0bb9a01f6b6cbe0d40
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-21 01:31:40 +0300

    New

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ wss.on("connection", function (ws) {
     //   cluster: "eu",
     // });
     // pusher.trigger("unic-messenger", "message", { message: "hello world" });
-    ws.send(req.body);
+    ws.send(JSON.parse(req.body));
     response.sendStatus(200);
   });
   ws.send("msg from server!");
