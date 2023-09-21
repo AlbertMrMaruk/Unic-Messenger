050d279d0e373d5f7c4993f1d748af1807a37492
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-21 13:58:03 +0300

    New

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -6,9 +6,63 @@ import clickChat from "../api/controlers/ChatsController";
 function Chats() {
   const [text, setText] = useState("");
   return (
-    <div className="bg-[#1b1d1f] h-max min-h-[100vh]  pb-10">
-      <h1 className="text-white text-3xl text-center pt-10">HELLO WORLD</h1>
+    <div className="bg-[#050505] flex h-max min-h-[100vh]">
+      <div
+        className="w-[28%] bg-inherit  border-r-[1px] border-[#2a2a2a] min-h-[100vh] p-[2rem]
+       "
+      >
+        <div className="flex  items-center justify-between w-[100%]">
+          <div className="bg-white rounded-full w-[45px] h-[45px]"></div>
+        </div>
+        <div
+          className="flex flex-col
+        items-center mt-[2rem]
+         "
+        >
+          <div
+            className="p-[1rem] 
+             border-[#2a2a2a] w-[100%] flex items-center gap-6 cursor-pointer hover:bg-[#1f2022]"
+          >
+            <div className="bg-white rounded-full w-[40px] h-[40px]"></div>
+            <div className="flex flex-col gap-2 text-white text-left">
+              <h3 className="text-md">Alex</h3>
+              <p className="text-xs">Text something</p>
+            </div>
+          </div>
+          <div
+            className="p-[1rem]  
+             border-[#2a2a2a] w-[100%] flex items-center gap-6 cursor-pointer hover:bg-[#1f2022]"
+          >
+            <div className="bg-white rounded-full w-[40px] h-[40px]"></div>
+            <div className="flex flex-col gap-2 text-white text-left">
+              <h3 className="text-md">Alex</h3>
+              <p className="text-xs">Text something</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="w-[72%] bg-inherit min-h-[100vh]
+       "
+      >
+        <div className="w-[100%] min-h-[90vh]"></div>
+        <div className="w-[100%] min-h-[10h] flex justify-center items-center">
+          <input className="bg-[#1c1d1f] text-white rounded-lg  w-[80%] p-2 h-[45px] mr-2" />
+          <button
+            className="bg-[#44a0ff] rounded-lg text-white p-1 text-xl w-[65px] h-[45px]
+        "
+            onClick={() => {
+              console.log(text);
+              setText("");
+              clickChat();
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
 
+      {/*       
       <div className="w-[70%] bg-slate-400 mt-[5rem] p-5 m-auto justify-end items-center flex flex-col min-h-[70vh]">
         <input
           className="bg-black text-white rounded-md w-[80%] p-2 "
@@ -26,7 +80,7 @@ function Chats() {
         >
           Send
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
