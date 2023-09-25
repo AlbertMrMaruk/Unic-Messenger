316eb1b65e4893762f24336de128289bec8fac80
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-25 12:02:06 +0300

    Git

--- src/components/blocks/Message.jsx
+++ src/components/blocks/Message.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Message({ message }) {
-  console.log(message);
+  console.log(message.payload);
   return (
     <div className="m-3 rounded-2xl bg-[#44a0ff] text-white px-3 py-2">
-      {message.text}
+      {message.payload.body}
     </div>
   );
 }
