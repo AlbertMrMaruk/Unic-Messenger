087b22a23c4993aae64f74986566af7dbe413a1c
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-26 13:35:37 +0300

    Git

--- src/components/blocks/Message.jsx
+++ src/components/blocks/Message.jsx
@@ -4,12 +4,12 @@ function Message({ message }) {
   return (
     <div
       className={`mx-3 mb-2 rounded-xl  text-white px-3 py-2 max-w-[45%] w-fit ${
-        message.event === "message"
+        message.event === "message" || message.fromMe === false
           ? "bg-[#2a2a2e] self-start"
           : "bg-[#44a0ff] self-end"
       }`}
     >
-      {message.payload.body}
+      {message?.payload?.body ?? message.body}
     </div>
   );
 }
