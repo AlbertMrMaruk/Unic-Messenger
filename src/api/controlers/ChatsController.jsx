23c1b3e301dd7cfd1f475ae177e8b60bffaab7f8
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-26 15:02:21 +0300

    Git

--- src/api/controlers/ChatsController.jsx
+++ src/api/controlers/ChatsController.jsx
@@ -18,7 +18,7 @@ function clickChat(setMessages) {
       //   console.log("Получены данные", event.data);
       //   console.log(messagesArr);
       //   console.log(messagesArr);
-      setMessages((prev) => [...prev, JSON.parse(event.data)]);
+      setMessages((prev) => [JSON.parse(event.data), ...prev]);
       //   console.log(JSON.parse(event.data));
     });
 
