b3603df8b03682b04bd0ca4b3364a6e5150ab1f3
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-28 23:44:10 +0300

    Git

--- src/components/blocks/Message.jsx
+++ src/components/blocks/Message.jsx
@@ -1,6 +1,16 @@
 function Message({ message }) {
   const url = message?.payload?.mediaUrl ?? message?.mediaUrl;
   const text = message?.payload?.body ?? message.body;
+  const timestamp = message?.payload?.timestamp ?? message.timestamp;
+  const calcDate = (timestamp) => {
+    let h = new Date(timestamp).getHours();
+    let m = new Date(timestamp).getMinutes();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+
+    return h + ":" + m;
+  };
   return (
     <div
       className={`mx-3 mb-2 rounded-xl  text-white px-3 py-2 max-w-[45%] w-fit ${
@@ -14,11 +24,12 @@ function Message({ message }) {
         <img
           src={"http://89.111.131.15" + url.slice(21)}
           alt="Image from user"
+          className="max-w-[300px]"
         />
       ) : (
         text
       )}
-      {}
+      <span className="text-right">{calcDate(timestamp)}</span>
     </div>
   );
 }
