073c1eeb04e505454a5212f357b375f8c2a89508
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-30 13:54:58 +0300

    Git

--- src/components/blocks/Tooltip.jsx
+++ src/components/blocks/Tooltip.jsx
@@ -10,27 +10,27 @@ export const Tooltip = ({ children }) => {
     >
       <span className="flex justify-center">{children}</span>
       <div
-        className={`absolute whitespace-nowrap bottom-full flex flex-col items-center  group-hover:flex ${
+        className={`absolute whitespace-nowrap bottom-full flex flex-col items-center   ${
           !show ? "hidden" : null
         }`}
       >
         <span className="relative z-10 p-4 text-xs leading-none text-white whitespace-no-wrap bg-[#1c1d1f] shadow-lg rounded-md flex gap-3">
-          <div className="flex flex-col gap-2 cursor-pointer">
+          <div className="flex flex-col gap-2 ">
             <FaFile color="#44a0ff" className="m-auto w-[25px] h-[25px]" />
             <span className="text-sm font-bold">Файл</span>
             <input
               type="file"
-              className=" w-[35px] h-[70px] absolute opacity-0 
+              className=" w-[35px] h-[70px] absolute opacity-0  cursor-pointer
             "
               accept="image/png, image/jpeg"
             />
           </div>
-          <div className="flex flex-col gap-2 cursor-pointer">
+          <div className="flex flex-col gap-2 ">
             <FaImage color="#44a0ff" className="m-auto w-[25px] h-[25px]" />
             <span className="text-sm font-bold">Фото</span>
             <input
               type="file"
-              className=" w-[35px] h-[70px] absolute opacity-0 
+              className=" w-[35px] h-[70px] absolute opacity-0  cursor-pointer
             "
               accept="image/png, image/jpeg"
             />
