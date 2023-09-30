d8c8418ed82a323c4e280e53ffa0814a5679a8a5
Marukyan Albert <albertmarukyan@yandex.ru>
2023-09-30 13:41:54 +0300

    Git

--- src/components/blocks/Tooltip.jsx
+++ src/components/blocks/Tooltip.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaFile } from "react-icons/fa";
+import { FaFile, FaImage } from "react-icons/fa";
 
 export const Tooltip = ({ children }) => {
   const [show, setShow] = useState(false);
@@ -17,8 +17,25 @@ export const Tooltip = ({ children }) => {
           !show ? "hidden" : null
         }`}
       >
-        <span className="relative z-10 p-4 text-xs leading-none text-white whitespace-no-wrap bg-[#1c1d1f] shadow-lg rounded-md">
-          <FaFile color="#44a0ff" />
+        <span className="relative z-10 p-4 text-xs leading-none text-white whitespace-no-wrap bg-[#1c1d1f] shadow-lg rounded-md flex gap-3">
+          <div className="flex flex-col gap-2">
+            <FaFile color="#44a0ff" />
+            <span className="text-sm">Файл</span>
+            <input
+              type="file"
+              className="invisible w-full
+            "
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <FaImage color="#44a0ff" />
+            <span className="text-sm">Фото</span>
+            <input
+              type="file"
+              className="invisible w-full
+            "
+            />
+          </div>
         </span>
         <div className="w-3 h-3 -mt-2 rotate-45 bg-[#1c1d1f] mb-2" />
       </div>
