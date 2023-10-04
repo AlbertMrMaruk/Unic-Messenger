9201375d5f9ced6d1dfa43a0a6098baf8cf0292d
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-04 18:19:37 +0300

    Git

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,43 +1,43 @@
-const mongoose = require("mongoose");
-const Product = mongoose.model("products");
+// const mongoose = require("mongoose");
+// const Product = mongoose.model("products");
 
-module.exports = (app) => {
-  app.get(`/api/user`, async (req, res) => {
-    console.log(req);
-    let products = await Product.find();
-    return res.status(200).send(products);
-  });
+// module.exports = (app) => {
+//   app.get(`/api/user`, async (req, res) => {
+//     console.log(req);
+//     let products = await Product.find();
+//     return res.status(200).send(products);
+//   });
 
-  app.post(`/api/product`, async (req, res) => {
-    console.log(req);
-    let product = await Product.create(req.body);
-    return res.status(201).send({
-      error: false,
-      product,
-    });
-  });
+//   app.post(`/api/product`, async (req, res) => {
+//     console.log(req);
+//     let product = await Product.create(req.body);
+//     return res.status(201).send({
+//       error: false,
+//       product,
+//     });
+//   });
 
-  //   app.put(`/api/product/:id`, async (req, res) => {
-  //     const {id} = req.params;
+//   //   app.put(`/api/product/:id`, async (req, res) => {
+//   //     const {id} = req.params;
 
-  //     let product = await Product.findByIdAndUpdate(id, req.body);
+//   //     let product = await Product.findByIdAndUpdate(id, req.body);
 
-  //     return res.status(202).send({
-  //       error: false,
-  //       product
-  //     })
+//   //     return res.status(202).send({
+//   //       error: false,
+//   //       product
+//   //     })
 
-  //   });
+//   //   });
 
-  //   app.delete(`/api/product/:id`, async (req, res) => {
-  //     const {id} = req.params;
+//   //   app.delete(`/api/product/:id`, async (req, res) => {
+//   //     const {id} = req.params;
 
-  //     let product = await Product.findByIdAndDelete(id);
+//   //     let product = await Product.findByIdAndDelete(id);
 
-  //     return res.status(202).send({
-  //       error: false,
-  //       product
-  //     })
+//   //     return res.status(202).send({
+//   //       error: false,
+//   //       product
+//   //     })
 
-  //   })
-};
+//   //   })
+// };
