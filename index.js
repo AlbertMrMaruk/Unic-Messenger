ea32782d911d20f8f2dcda6ec28a78f54d1be7ff
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-04 22:10:35 +0300

    Git

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const http = require("http");
 const Websocket = require("ws");
-// const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 console.log("Hello server");
 
 // require(__dirname + "/models/User");
 
 // mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://89.111.131.15:27017/local`, {
+  authSource: "admin",
+  useNewUrlParser: true,
+  user: "admin",
+  pass: "ALBert2002",
+  serverSelectionTimeoutMS: 5000,
+});
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 // console.log(db);
-// db.on("error", () => {
-//   console.log("> error occurred from the database");
-// });
-// db.once("open", () => {
-//   console.log("> successfully opened the database");
-// });
+db.on("error", () => {
+  console.log("> error occurred from the database");
+});
+db.once("open", () => {
+  console.log("> successfully opened the database");
+});
 // require(__dirname + "/routes/userRoutes")(app);
 app.use(express.static(__dirname + "/build/"));
 app.use(bodyParser.json());
@@ -53,8 +60,6 @@ app.post("/post", function (req, response) {
 
   response.sendStatus(200);
 });
-
-// mongoose.connect(`mongodb://127.0.0.1:27017/messenger`);
 
 server.listen(3002, function () {
   console.log(`Приложение запущено на порту  ${3002}!`);
