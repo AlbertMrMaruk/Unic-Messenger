4a44450bec3a4dcdc3693e17799f77b0862e68db
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-06 16:12:26 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -76,7 +76,7 @@ function Chats({ messages, setMessages }) {
                     delete el._data;
                     return el;
                   });
-                  if (index === 29) {
+                  if (index === res.length - 1) {
                     fetch(`http://89.111.131.15/database/users`, {
                       method: "post",
                       headers: {
