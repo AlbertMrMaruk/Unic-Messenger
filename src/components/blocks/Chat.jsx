93fd5724293340d73cf39157a0963462fc0ca062
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-10 15:18:41 +0300

    Git

--- src/components/blocks/Chat.jsx
+++ src/components/blocks/Chat.jsx
@@ -26,7 +26,7 @@ border-[#2a2a2a] w-[100%] rounded-xl flex items-center gap-6 cursor-pointer hove
           />
         )}
       </div>
-      <div className="flex flex-col gap-1 text-[#e9e9e9] text-left max-w-[50%]">
+      <div className="flex flex-col gap-1 text-[#e9e9e9] text-left max-w-[70%]">
         <h3 className="text-md">{chat.name}</h3>
         <p className="text-[0.85rem] text-[#777779]">
           {chat.lastMessage.fromMe ? "Вы: " : ""}
@@ -36,7 +36,7 @@ border-[#2a2a2a] w-[100%] rounded-xl flex items-center gap-6 cursor-pointer hove
         </p>
       </div>
       {chat.unreadCount !== 0 && (
-        <div className="bg-[#44a0ff] text-white m-auto text-center p-4 ">
+        <div className="bg-[#44a0ff] text-white m-auto text-center px-[.6rem] py-[.2rem] rounded-full text-[14px]  font-bold">
           {chat.unreadCount}
         </div>
       )}
