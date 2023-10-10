18c9b1b026060be49413a0af551b11ec65029675
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-10 17:13:03 +0300

    Git

--- src/api/ChatsAPI.jsx
+++ src/api/ChatsAPI.jsx
@@ -21,6 +21,34 @@ class ChatsAPI {
       console.log("Err", error);
     }
   }
+  startTyping(phone, session) {
+    return fetch(`http://89.111.131.15/api/startTyping`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        session,
+        chatId: `${phone}`,
+      }),
+    });
+  }
+  stopTyping(phone, session) {
+    return fetch(`http://89.111.131.15/api/stopTyping`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        session,
+        chatId: `${phone}`,
+      }),
+    });
+  }
   getMessages(id, limit, session) {
     return fetch(
       `http://89.111.131.15/api/${session}/chats/${id}/messages?downloadMedia=true&limit=${limit}`
