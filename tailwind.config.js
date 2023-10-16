2d91ec92aecf8144fddb39acef0e958906d361aa
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-16 10:14:43 +0300

    Git

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      white: "#ffffff",
+      black: "#000000",
       primary: "#44a0ff",
       secondary: "#1b1d1f",
       secondarylight: "#242527",
