3d2ba3fd161c62faaae8520b2c5ee321af36a42c
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-16 23:59:45 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -457,7 +457,7 @@ border-[#2a2a2a] w-[100%] rounded-xl flex items-center gap-6 cursor-pointer hove
           </div>
         )}
         {/* Messages in chat */}
-        <div className="w-[100%] flex-col-reverse py-3  flex items-start justify-start px-[.25rem] md:px-[2.5rem] overflow-scroll h-[70vh] md:h-[80vh] mt-2">
+        <div className="w-[100%] flex-col-reverse py-3  flex items-start justify-start px-[.25rem] md:px-[2.5rem] overflow-scroll h-[70vh] md:h-[75vh] mt-2">
           {showSpinnerMessages ? (
             <Spinner />
           ) : (
@@ -469,14 +469,16 @@ border-[#2a2a2a] w-[100%] rounded-xl flex items-center gap-6 cursor-pointer hove
           )}
         </div>
         {/* Input and buttons for send messages */}
-        <div className="w-[100%] h-[7h]  justify-center items-center">
+        <div className="w-[100%] h-[9h]  justify-center items-center">
           {replyMessage && (
-            <div className="w-[95%] md:w-[90%] flex bg-secondarylight bg-opacity-20 rounded-t-xl m-auto py-2 px-4">
-              <FaReply className="w-[35px] mr-3" />
-              <span className="text-sm">
-                {replyMessage.body ?? replyMessage.payload.body}
-              </span>
-              <FaWindowClose className="w-[35px]  justify-end " />
+            <div className="w-[95%] md:w-[90%] flex bg-secondarylight bg-opacity-70 rounded-t-xl m-auto py-3 px-4 justify-between items-center">
+              <div>
+                <FaReply className="w-[30px] mr-3" />
+                <span className="text-md">
+                  {replyMessage.body ?? replyMessage.payload.body}
+                </span>
+              </div>
+              <FaWindowClose className="w-[40px]" />
             </div>
           )}
           <div className="relative flex flex-wrap items-stretch m-auto w-[95%] md:w-[90%]">
