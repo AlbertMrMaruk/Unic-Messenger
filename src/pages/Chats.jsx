46f533c56f68ea2b3c52a2ebd4ff12033b14d378
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-16 14:11:06 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -302,6 +302,14 @@ function Chats() {
           ]);
         });
       } else {
+        setMessages((prev) => [
+          {
+            payload: { body: text },
+            event: "send",
+            timestamp: Date.now(),
+          },
+          ...prev,
+        ]);
         ChatsApi.sendText(text, currentChat, session).then(() => {
           chats[chatIndex].lastMessage = {
             body: text,
@@ -317,14 +325,7 @@ function Chats() {
               timestamp: Date.now(),
             },
           ];
-          setMessages((prev) => [
-            {
-              payload: { body: text },
-              event: "send",
-              timestamp: Date.now(),
-            },
-            ...prev,
-          ]);
+
           setChats((prev) =>
             prev.sort((chat1, chat2) => {
               const chat1time =
