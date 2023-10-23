43902ed6b947d53e93d8cbf86b774aa0755d24a4
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-23 11:56:40 +0300

    Git

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,10 +13,10 @@ export default function Modal({
   const [showSpinner, setShowSpinner] = useState(false);
   return (
     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-[rgba(0,0,0,.7)]">
-      <div className="relative my-6 mx-auto w-[50%]">
+      <div className="relative my-6 mx-auto w-[90%] md:w-[50%]">
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-secondarylight outline-none focus:outline-none">
           <div className="flex items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
-            <h3 className="text-3xl font-bold text-white m-auto">
+            <h3 className="md:text-3xl font-bold text-white m-auto text-center md:text-left text-[1.65rem]">
               Отправить файл
             </h3>
           </div>
