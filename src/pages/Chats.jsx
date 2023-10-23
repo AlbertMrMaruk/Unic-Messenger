a2d7debe9ccacfe402f745e8c4d70979d8bd0ef0
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-23 15:00:23 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -102,8 +102,10 @@ function Chats() {
 
       setChats((prev) =>
         prev.sort((chat1, chat2) => {
-          const chat1time = +(chat1?.lastMessage?.payload?.timestamp + "000");
-          const chat2time = +(chat2?.lastMessage?.payload?.timestamp + "000");
+          const chat1time = +chat1?.lastMessage?.timestamp;
+
+          const chat2time = +chat2?.lastMessage?.timestamp;
+
           console.log(chat1, chat2, chat1time, chat2time);
           return chat1time > chat2time ? -1 : 1;
         })
