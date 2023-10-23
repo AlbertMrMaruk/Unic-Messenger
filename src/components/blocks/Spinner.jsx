db13de4913b88a4b3404081a81abb75d9e83dd5b
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-23 16:35:55 +0300

    Git

--- src/components/blocks/Spinner.jsx
+++ src/components/blocks/Spinner.jsx
@@ -2,7 +2,7 @@ import spinner from "../../assets/785-4.gif";
 
 function Spinner() {
   return (
-    <div className="m-auto h-max pb-10 shadow-inner py-[60%] md:py-[20%]">
+    <div className="m-auto h-max pb-10 shadow-inner  py-[30%] md:py-[20%]">
       <img className="m-auto w-[64px]" alt="Spinner" src={spinner} />
     </div>
   );
