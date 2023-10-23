77698a93ee032e2c218b29bd834da3b76cd2f4db
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-23 16:08:51 +0300

    Git

--- src/components/blocks/Message.jsx
+++ src/components/blocks/Message.jsx
@@ -25,7 +25,7 @@ function Message({ message }) {
   };
   return (
     <div
-      className={`mx-3 mb-2 rounded-xl  text-white pr-2 pl-3 min-w-[9%] py-2 max-w-[45%] w-fit flex flex-col gap-1 ${
+      className={`mx-3 mb-2 rounded-xl  text-white pr-2 pl-3 min-w-[9%] py-2 max-w-[70%] md:max-w-[45%] w-fit flex flex-col gap-1 ${
         !message?.fromMe ? "bg-[#2a2a2e] self-start" : "bg-primary self-end"
       }`}
     >
