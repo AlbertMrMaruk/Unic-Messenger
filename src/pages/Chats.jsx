0e37cc59ebcb94c74be78b8a90b8b50c783f700d
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-25 01:42:23 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -234,42 +234,8 @@ function Chats() {
                   return el;
                 });
                 setPercentage((prev) => +prev + 2);
-                // data.chatsCount += 1;
-
-                // setPercentage((prev) => +prev + 1);
-                // ChatsApi.getAvatar(el.id.user, userData[0].accounts[0])
-                //   .then((el) => el.json())
-                //   .then((el) => {
-                //     console.log(el?.profilePictureURL);
-                //     data.chats[index].avatar = el?.profilePictureURL;
-
-                //     if (data.chatsCount === 30) {
-                //       console.log(allSize, "and nooooowwww");
-                //       data.allSize = allSize;
-                //       setPercentage(100);
-                //       DatabaseAPI.updateUser(userData[0].username, {
-                //         chats: data.chats,
-                //         allSize: data.allSize,
-                //         accounts: data.accounts,
-                //       })
-                //         .then((res) => res.json())
-                //         .then((res) => {
-                //           console.log(res);
-                //           dataToApp(data);
-
-                //           setShowModalDownload(false);
-                //         });
-                //     }
-                //   });
-              });
-            await ChatsApi.getAvatar(el.id.user, userData[0].accounts[0])
-              .then((el) => el.json())
-              .then((el) => {
-                console.log(el?.profilePictureURL);
-                data.chats[index].avatar = el?.profilePictureURL;
-                setPercentage((prev) => +prev + 1);
                 data.chatsCount += 1;
-                if (data.chatsCount === 30) {
+                if (data.chatsCount === 40) {
                   console.log(allSize, "and nooooowwww");
                   data.allSize = allSize;
                   setPercentage(100);
@@ -287,11 +253,38 @@ function Chats() {
                     });
                 }
               });
+            // Загрузка иконки профиля
+            // await ChatsApi.getAvatar(el.id.user, userData[0].accounts[0])
+            //   .then((el) => el.json())
+            //   .then((el) => {
+            //     console.log(el?.profilePictureURL);
+            //     data.chats[index].avatar = el?.profilePictureURL;
+            //     setPercentage((prev) => +prev + 1);
+            //     data.chatsCount += 1;
+            // if (data.chatsCount === 30) {
+            //   console.log(allSize, "and nooooowwww");
+            //   data.allSize = allSize;
+            //   setPercentage(100);
+            //   DatabaseAPI.updateUser(userData[0].username, {
+            //     chats: data.chats,
+            //     allSize: data.allSize,
+            //     accounts: data.accounts,
+            //   })
+            //     .then((res) => res.json())
+            //     .then((res) => {
+            //       console.log(res);
+            //       dataToApp(data);
+
+            //       setShowModalDownload(false);
+            //     });
+            // }
+            // });
           };
           for (let i = 0; i < 40; i++) {
             await fetchChat(data.chats[i], i);
-            if (i === 20) {
-              await delay(5000);
+            if (i === 10) {
+              console.log("Delay");
+              await delay(3000);
             }
           }
         });
