e79e3fb7de45d044af844a4913e353c119beea85
Marukyan Albert <albertmarukyan@yandex.ru>
2023-10-26 12:59:07 +0300

    Git

--- src/components/blocks/TooltipVoice.jsx
+++ src/components/blocks/TooltipVoice.jsx
@@ -83,7 +83,7 @@ function TooltipVoice({ children }) {
       className="relative flex flex-col  group "
       onClick={() => {
         console.log("gmgmg");
-        setShow(false);
+        setShow(true);
         // const onClick = () => {
         //   setShow(false);
         //   window.removeEventListener("click", onClick);
