aae44c99bc5796cfa6f98564a1ce425b85f5841c
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-07 21:24:38 +0300

    Git

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -221,9 +221,7 @@ function Chats() {
       setShowModalAccount(true);
     } else if (
       userData[0].accounts.length > 0 &&
-      (userData[0].chats?.[session]?.length === 0 ||
-        Object.keys(userData[0].chats)?.length === 0 ||
-        userData[0].chats?.length === 0)
+      (userData[0].chats?.[session]?.length === 0 || !userData[0].chats)
     ) {
       let currentSession = session ?? userData[0].accounts[0];
       setSession(currentSession);
