15bd1de708c6549024ac7b190eb0ae62df9f1668
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-07 14:56:28 +0300

    Git

--- src/components/blocks/Message.jsx
+++ src/components/blocks/Message.jsx
@@ -36,7 +36,7 @@ function Message({ message }) {
     <div
       className={`mx-3 mb-2 rounded-xl  text-white pr-2 pl-3 min-w-[9%] py-2   flex flex-col gap-1 ${
         !message?.fromMe ? "bg-[#2a2a2e] self-start" : "bg-primary self-end"
-      } ${isAudio(url) ? "w-[50%]" : " max-w-[70%] md:max-w-[45%] w-fit"}`}
+      } ${isAudio(url) ? "w-[40%]" : " max-w-[70%] md:max-w-[45%] w-fit"}`}
     >
       {/* Проверка файла на тип если пдф или видео */}
       {isVideo(url) && (
