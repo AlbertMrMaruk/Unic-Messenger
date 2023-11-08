faacc74ce80e4602e300631b44179eadd36e6f6c
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-08 17:38:09 +0300

    Git

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   accounts: Array,
-  chats: {},
+  chats: { default: [] },
   allSize: Number,
   chatsCount: Number,
 });
