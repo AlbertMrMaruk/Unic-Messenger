9f711db1d4824122fa8b967f0d73a0276e6db451
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-13 12:51:16 +0300

    Git

--- src/components/ModalChats.jsx
+++ src/components/ModalChats.jsx
@@ -30,14 +30,14 @@ export default function ModalChats({
     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-[rgba(0,0,0,.7)]">
       <div className="relative my-6 mx-auto w-[90%] md:w-[50%]">
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-secondarylight outline-none focus:outline-none">
-          <div className="flex flex-col items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
+          <div className="flex flex-col items-start justify-between p-5  rounded-t ">
             <h3 className="md:text-3xl font-bold text-white m-auto text-center md:text-left text-[1.65rem]">
               Создать новый чат
             </h3>
             <input
               type="text"
               className="
-             bg-[#cdcdcd] text-black rounded-[5px]  font-bold  px-[.5rem] py-[.2rem] h-[35px]  relative  block w-[90%] mx-auto mt-[1.5rem] mb-[.5rem] text-[14px] outline-none  "
+             bg-[#cdcdcd] text-black rounded-[5px]  font-bold  px-[.5rem] py-[.5rem] h-[35px]  relative  block w-[90%] mx-auto mt-[1.5rem] mb-[.5rem] text-[14px] outline-none  "
               value={queryContact}
               onChange={(e) => {
                 setFiltContacts(
@@ -55,7 +55,7 @@ export default function ModalChats({
           {showSpinner ? (
             <Spinner />
           ) : (
-            <div className="relative p-2 ml-2 flex-auto text-center overflow-scroll h-[50vh]">
+            <div className="relative p-2 ml-2 flex-auto text-center overflow-scroll h-[40vh]">
               {filtContacts?.map(
                 (el) =>
                   el?.isWAContact && (
