7bf41a4a7bed4d189b35243ed243bc2032c4da6d
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-13 11:59:30 +0300

    Git

--- src/components/ModalChats.jsx
+++ src/components/ModalChats.jsx
@@ -30,32 +30,41 @@ export default function ModalChats({
     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-[rgba(0,0,0,.7)]">
       <div className="relative my-6 mx-auto w-[90%] md:w-[50%]">
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-secondarylight outline-none focus:outline-none">
-          <div className="flex items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
+          <div className="flex col items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
             <h3 className="md:text-3xl font-bold text-white m-auto text-center md:text-left text-[1.65rem]">
               Создать новый чат
             </h3>
+            <input
+              type="text"
+              className="
+             bg-[#cdcdcd] text-black rounded-[5px]  font-bold  px-[.5rem] py-[.2rem] h-[35px]  relative  block w-[350px] mx-auto my-[1.5rem] text-[14px] outline-none  "
+              value={queryContact}
+              onChange={(e) => {
+                console.log(
+                  contacts.filter(
+                    (el) =>
+                      `${el?.name}`.includes(e.target.value) ||
+                      `${el?.pushName}`.includes(e.target.value) ||
+                      `${el?.number}`.includes(e.target.value)
+                  ),
+                  e.target.value
+                );
+                // setFiltContacts(
+                //   contacts.filter(
+                //     (el) =>
+                //       `${el?.name}`.includes(e.target.value) ||
+                //       `${el?.pushName}`.includes(e.target.value) ||
+                //       `${el?.number}`.includes(e.target.value)
+                //   )
+                // );
+                setQueryContact(e.target.value);
+              }}
+            />
           </div>
           {showSpinner ? (
             <Spinner />
           ) : (
             <div className="relative p-2 ml-2 flex-auto text-center overflow-scroll h-[50vh]">
-              <input
-                type="text"
-                className="
-             bg-[#cdcdcd] text-black rounded-[5px]  font-bold  px-[.5rem] py-[.2rem] h-[35px]  relative  block w-[350px] mx-auto my-[1rem] text-[14px] outline-none  "
-                value={queryContact}
-                onChange={(e) => {
-                  setFiltContacts(
-                    contacts.filter(
-                      (el) =>
-                        `${el?.name}`.includes(e.value) ||
-                        `${el?.pushName}`.includes(e.value) ||
-                        `${el?.number}`.includes(e.value)
-                    )
-                  );
-                  setQueryContact(e.value);
-                }}
-              />
               {filtContacts?.map(
                 (el) =>
                   el?.isWAContact && (
