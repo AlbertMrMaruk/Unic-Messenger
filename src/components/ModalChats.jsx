164d02d3c3108e6d0c090493654f3ffbf83414ae
Marukyan Albert <albertmarukyan@yandex.ru>
2023-11-13 12:01:34 +0300

    Git

--- src/components/ModalChats.jsx
+++ src/components/ModalChats.jsx
@@ -24,13 +24,13 @@ export default function ModalChats({
         setFiltContacts(el);
         setShowSpinner(false);
       });
-  });
+  }, []);
 
   return (
     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-[rgba(0,0,0,.7)]">
       <div className="relative my-6 mx-auto w-[90%] md:w-[50%]">
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-secondarylight outline-none focus:outline-none">
-          <div className="flex col items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
+          <div className="flex flex-col items-start justify-between p-5 border-b border-solid border-[#2a2a2a] rounded-t ">
             <h3 className="md:text-3xl font-bold text-white m-auto text-center md:text-left text-[1.65rem]">
               Создать новый чат
             </h3>
@@ -40,23 +40,14 @@ export default function ModalChats({
              bg-[#cdcdcd] text-black rounded-[5px]  font-bold  px-[.5rem] py-[.2rem] h-[35px]  relative  block w-[350px] mx-auto my-[1.5rem] text-[14px] outline-none  "
               value={queryContact}
               onChange={(e) => {
-                console.log(
+                setFiltContacts(
                   contacts.filter(
                     (el) =>
                       `${el?.name}`.includes(e.target.value) ||
                       `${el?.pushName}`.includes(e.target.value) ||
                       `${el?.number}`.includes(e.target.value)
-                  ),
-                  e.target.value
+                  )
                 );
-                // setFiltContacts(
-                //   contacts.filter(
-                //     (el) =>
-                //       `${el?.name}`.includes(e.target.value) ||
-                //       `${el?.pushName}`.includes(e.target.value) ||
-                //       `${el?.number}`.includes(e.target.value)
-                //   )
-                // );
                 setQueryContact(e.target.value);
               }}
             />
